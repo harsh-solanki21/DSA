6e4120bbec4ca28b9e787ded881d23032d569224
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-07-22 11:39:14 +0530

    Subarrays

--- Arrays/Subarrays.java
+++ Arrays/Subarrays.java
@@ -6,22 +6,23 @@ import java.util.List;
 public class Subarrays {
 
     // Recursive Approach - O(n^2)
-    static void subArraysRecursive(int[] nums, int start, int end) {
-        if (end == nums.length)
+    static void subarraysRecursive(int[] nums, int start, int end) {
+        if (start == nums.length) {
             return;
-        else if (start > end)
-            subArraysRecursive(nums, 0, end + 1);
-        else {
-            for (int i = start; i <= end; i++) {
-                System.out.print(nums[i] + " ");
-            }
-            System.out.println();
-            subArraysRecursive(nums, start + 1, end);
         }
+        if (end == nums.length) {
+            subarraysRecursive(nums, start + 1, start + 1);
+            return;
+        }
+        for (int i = start; i <= end; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+        subarraysRecursive(nums, start, end + 1);
     }
 
     // Iterative Approach - O(n^3)
-    static void subArrays(int[] nums) {
+    static void subarrays(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i; j < nums.length; j++) {
                 for (int k = i; k <= j; k++) {
@@ -49,8 +50,8 @@ public class Subarrays {
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4};
-        subArraysRecursive(nums, 0, 0);
-        subArrays(nums);
+        subarraysRecursive(nums, 0, 0);
+        subarrays(nums);
         System.out.println(findSubarrays(nums));
     }
 
