10400018fe17c44d77945197d2353987b35714a2
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-07-22 11:15:42 +0530

    Subarrays

--- Arrays/Subarrays.java
+++ Arrays/Subarrays.java
@@ -1,8 +1,26 @@
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Subarrays {
 
-    // Iterative Approach   O(n^3)
+    // Recursive Approach - O(n^2)
+    static void subArraysRecursive(int[] nums, int start, int end) {
+        if (end == nums.length)
+            return;
+        else if (start > end)
+            subArraysRecursive(nums, 0, end + 1);
+        else {
+            for (int i = start; i <= end; i++) {
+                System.out.print(nums[i] + " ");
+            }
+            System.out.println();
+            subArraysRecursive(nums, start + 1, end);
+        }
+    }
+
+    // Iterative Approach - O(n^3)
     static void subArrays(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i; j < nums.length; j++) {
@@ -14,27 +32,26 @@ public class Subarrays {
         }
     }
 
-    // Recursive Approach   O(n^2)
-    static void subArraysRecursive(int[] nums, int start, int end) {
-        if (end == nums.length)
-            return;
-        else if (start > end)
-            subArraysRecursive(nums, 0, end + 1);
-        else {
-//            System.out.print("[");
-            for (int i = start; i <= end; i++) {
-                System.out.print(nums[i] + " ");
+    // Iterative Approach - O(n^2)
+    public static List<List<Integer>> findSubarrays(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = i; j < nums.length; j++) {
+                list.add(nums[j]);
+                ans.add(new ArrayList<>(list));
             }
-            System.out.println();
-//            System.out.println(arr[end] + "]");
-            subArraysRecursive(nums, start + 1, end);
         }
+
+        return ans;
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4};
+        subArraysRecursive(nums, 0, 0);
         subArrays(nums);
-//        subArraysRecursive(nums, 0, 0);
+        System.out.println(findSubarrays(nums));
     }
 
 }
