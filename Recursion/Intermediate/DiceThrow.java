5cb1a1ecf98073c299dad5d83a7b4873132568d3
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-07-26 17:51:24 +0530

    Number of Dice Rolls With Target Sum

--- Recursion/Intermediate/DiceThrow.java
+++ Recursion/Intermediate/DiceThrow.java
@@ -57,11 +57,34 @@ public class DiceThrow {
         return list;
     }
 
+
+    // https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/description/
+    static int MOD = 1000000007;
+
+    static int numRollsToTarget(int n, int k, int target) {
+        if (target < 0 || n == -1) {
+            return 0;
+        }
+        if (target == 0) {
+            return 1;
+        }
+
+        int ans = 0;
+        for (int i = 1; i <= k; i++) {
+            ans = (ans + numRollsToTarget(n - 1, k, target - i) % MOD) % MOD;
+        }
+        return ans;
+    }
+
+
     public static void main(String[] args) {
 //        dice("", 4);
 //        System.out.println(diceRet("", 4));
 //        diceCustom("", 5, 8);
-        System.out.println(diceCustomRet("", 4, 5));
+//        System.out.println(diceCustomRet("", 4, 5));
+
+        int n = 2, k = 6, target = 7;
+        System.out.println(numRollsToTarget(n, k, target)); // 6
     }
 
 }
