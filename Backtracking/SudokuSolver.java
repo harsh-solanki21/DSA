17d80a64cbf65ed305665dba53296e86800c9dee
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-07-29 17:15:37 +0530

    Sudoku Solver

--- Backtracking/SudokuSolver.java
+++ Backtracking/SudokuSolver.java
@@ -4,59 +4,59 @@ import java.util.Arrays;
 
 public class SudokuSolver {
 
+    // https://leetcode.com/problems/sudoku-solver/description/
+
     // TC - O(9^n^2)
-    static void solveSudoku(int[][] board, int i, int j) {
-        if (i == board.length) {
-            for (int[] nums : board) {
-                System.out.println(Arrays.toString(nums));
-            }
-            return;
-        }
+    // SC - O(n^2)
 
-        int row = 0, col = 0;
-        if (j == board[0].length - 1) {
-            row = i + 1;
-            col = 0;
-        } else {
-            row = i;
-            col = j + 1;
-        }
-
-        if (board[i][j] != 0) {
-            solveSudoku(board, row, col);
-        } else {
-            for (int k = 1; k <= 9; k++) {
-                if (isValid(board, i, j, k)) {
-                    board[i][j] = k;
-                    solveSudoku(board, row, col);
-                    board[i][j] = 0;
-                }
-            }
-        }
+    static void solveSudoku(char[][] board) {
+        solve(board, 0, 0);
     }
 
-    private static boolean isValid(int[][] board, int row, int col, int val) {
-        for (int j = 0; j < board[0].length; j++) {
-            if (board[row][j] == val) {
-                return false;
-            }
+    private static boolean solve(char[][] board, int row, int col) {
+        if (row == board.length) {
+            return true;
         }
 
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][col] == val) {
-                return false;
-            }
+        if (col == board[0].length) {
+            return solve(board, row + 1, 0);
         }
 
-        // get the corner of submatrix
-        int smi = row / 3 * 3;  // sub matrix i
-        int smj = col / 3 * 3;  // sub matrix j
+        if (board[row][col] != '.') {
+            return solve(board, row, col + 1);
+        }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[smi + i][smj + j] == val) {
-                    return false;
+        for (char i = '1'; i <= '9'; i++) {
+            if (isValidPlacement(board, row, col, i)) {
+                board[row][col] = i;
+                if (solve(board, row, col + 1)) {
+                    return true;
                 }
+                board[row][col] = '.';
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isValidPlacement(char[][] board, int row, int col, char num) {
+        for (int i = 0; i < board.length; i++) {
+            // Check row
+            if (board[i][col] == num) {
+                return false;
+            }
+
+            // Check column
+            if (board[row][i] == num) {
+                return false;
+            }
+
+            // Check 3x3 subgrid
+            int subgridRow = 3 * (row / 3) + i / 3; // Calculate row index of subgrid
+            int subgridCol = 3 * (col / 3) + i % 3; // Calculate column index of subgrid
+
+            if (board[subgridRow][subgridCol] == num) {
+                return false;
             }
         }
 
@@ -65,21 +65,18 @@ public class SudokuSolver {
 
 
     public static void main(String[] args) {
-        int[][] board = new int[][]{
-                {3, 0, 6, 5, 0, 8, 4, 0, 0},
-                {5, 2, 0, 0, 0, 0, 0, 0, 0},
-                {0, 8, 7, 0, 0, 0, 0, 3, 1},
-                {0, 0, 3, 0, 1, 0, 0, 8, 0},
-                {9, 0, 0, 8, 6, 3, 0, 0, 5},
-                {0, 5, 0, 0, 9, 0, 6, 0, 0},
-                {1, 3, 0, 0, 0, 0, 2, 5, 0},
-                {0, 0, 0, 0, 0, 0, 0, 7, 4},
-                {0, 0, 5, 2, 0, 6, 3, 0, 0}
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
         };
-
-        solveSudoku(board, 0, 0);
-
+        solveSudoku(board);
     }
-
 
 }
