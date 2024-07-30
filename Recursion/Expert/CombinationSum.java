05c03b83d3e92f6be997f473385d97fd692aee8b
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-07-30 16:34:03 +0530

    Combination Sum

--- Recursion/Expert/CombinationSum.java
+++ Recursion/Expert/CombinationSum.java
@@ -12,31 +12,6 @@ public class CombinationSum {
     // Therefore, the overall time complexity of the given code is O(2^n * n).
 
 
-    // Unique Combination Sum
-    static List<List<Integer>> uniqueCombinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        uniqueCombinations(candidates, target, 0, ans, new ArrayList<>());
-        return ans;
-    }
-
-    private static void uniqueCombinations(int[] candidates, int target, int index, List<List<Integer>> ans, List<Integer> list) {
-        if (target == 0) {
-            ans.add(new ArrayList<>(list));
-            return;
-        }
-        if (target < 0 || index == candidates.length) {
-            return;
-        }
-
-        if (candidates[index] <= target) {
-            list.add(candidates[index]);
-            uniqueCombinations(candidates, target - candidates[index], index, ans, list);
-            list.remove(list.size() - 1);
-        }
-        uniqueCombinations(candidates, target, index + 1, ans, list);
-    }
-
-
     // Not Unique Combinations
     static List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
@@ -60,12 +35,34 @@ public class CombinationSum {
         }
     }
 
+    // Unique Combinations Sum
+    static List<List<Integer>> uniqueCombinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        uniqueCombinations(candidates, target, 0, ans, new ArrayList<>());
+        return ans;
+    }
+
+    private static void uniqueCombinations(int[] candidates, int target, int index, List<List<Integer>> ans, List<Integer> list) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        if (target < 0 || index == candidates.length) {
+            return;
+        }
+
+        list.add(candidates[index]);
+        uniqueCombinations(candidates, target - candidates[index], index, ans, list);
+        list.remove(list.size() - 1);
+        uniqueCombinations(candidates, target, index + 1, ans, list);
+    }
+
 
     public static void main(String[] args) {
-        int[] candidates = {2, 3, 5};
-        int target = 8;
-        System.out.println(uniqueCombinationSum(candidates, target));
+        int[] candidates = {2, 3, 6, 7};
+        int target = 7;
         System.out.println(combinationSum(candidates, target));
+        System.out.println(uniqueCombinationSum(candidates, target));
     }
 
 }
